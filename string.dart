cc5fdff2505ab9eb96c00082b3531aa83f26d145
TopuFD <topuroy3317@gmail.com>
2024-07-25 21:43:13 +0600

    add new

---
+++ string.dart
@@ -0,0 +1,22 @@
+void main() {
+  //practice string propertice=========
+
+  String txt = "topu";
+  print(txt.codeUnits);
+  print(txt.runes);
+  print(txt.isEmpty);
+  print(txt.isNotEmpty);
+  print(txt.length);
+  //practice string propertice=========
+
+  //practice string methods===============
+  String text = "My Janu Nipa  ";
+  String myText1 = "topu";
+  String myText2 = "Hr";
+  print(text.toUpperCase());
+  print(text.toLowerCase());
+  print(text.trim());
+  print(myText1.compareTo(myText2));
+  print(text.split(' '));
+  print(text.substring(8,13));
+}
