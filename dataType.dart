f698546380695b79e91cccae8013ee9907cc0319
TopuFD <topuroy3317@gmail.com>
2024-07-25 21:28:22 +0600

    add some

--- dataType.dart
+++ dataType.dart
@@ -1,21 +1,23 @@
 import 'list.dart';
 
 void main() {
-  // Parson personObj = new Parson();
+  Parson personObj = new Parson();
 
   // dart optionaly typed language=============
-  // dynamic number = 10;
-  // print("data type of number before changing the value: ${number.runtimeType}");
-  // number = "topu";
+  dynamic number = 10;
+  print("data type of number before changing the value: ${number.runtimeType}");
+  number = "topu";
 
-  // print("data type of number after changing the value: ${number.runtimeType}");
+  print("data type of number after changing the value: ${number.runtimeType}");
 
   // dart optionaly typed language=============
 
-  var value1 = 10;
-  var value2 = 10;
-  print(value1 != value2);
+//   var value1 = 10;
+//   var value2 = 10;
+//   print(value1 != value2);
 
-  var later = " ";
-  print(later.runes);
+//   var later = " ";
+//   print(later.runes);
+
+  
 }
