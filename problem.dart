a0fa48a868ef2b0739b68218829d75a979febeec
TopuFD <topuroy3317@gmail.com>
2024-07-26 04:15:45 +0600

    add new

---
+++ problem.dart
@@ -0,0 +1,19 @@
+void main() {
+  // problem 1 ===> removing allo white space from a string
+  // String text = "i am Topu. a dadicated flutter developer";
+  // print(text.replaceAll(" ", ""));
+
+  // problem 2: swaping problem solving
+
+  //Suppose, your distance to office from home is 25 km and you travel 40 km per hour. Write a program to calculate time taken to reach office in minutes. Formula= (distance) / (speed)
+
+  //find greatest number from three numbers
+  // int num1 = 400;
+  // int num2 = 352;
+  // int num3 = 500;
+  var age = 20;
+  assert(
+    age != age,
+    "error"
+  );
+}
