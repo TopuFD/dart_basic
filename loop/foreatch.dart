6b16a42e541e9bfbc4a7d8c7a4ce6c43de170696
TopuFD <topuroy3317@gmail.com>
2024-07-26 04:13:31 +0600

    add new practice

---
+++ loop/foreatch.dart
@@ -0,0 +1,6 @@
+void main() {
+  List<String> players = ['Ronaldo', 'Messi', 'Neymar', 'Hazard'];
+  players.forEach((value) {
+    print(value);
+  });
+}
---
+++ loop/forloop.dart
@@ -0,0 +1,6 @@
+//print one to houndread by for loop
+void main() {
+  for (var i = 1; i <= 100; i++) {
+    print(i);
+  }
+}
