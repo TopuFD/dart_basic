ddcdd4b1e233dbfb04015028f272d46c403832bd
TopuFD <topuroy3317@gmail.com>
2024-07-26 20:24:40 +0600

    solve new problem

--- problem.dart
+++ problem.dart
@@ -8,12 +8,17 @@ void main() {
   //Suppose, your distance to office from home is 25 km and you travel 40 km per hour. Write a program to calculate time taken to reach office in minutes. Formula= (distance) / (speed)
 
   //find greatest number from three numbers
-  // int num1 = 400;
-  // int num2 = 352;
-  // int num3 = 500;
-  var age = 20;
-  assert(
-    age != age,
-    "error"
-  );
+  int num1 = 700;
+  int num2 = 652;
+  int num3 = 500;
+
+  if (num1 > num2) {
+    if (num1 > num3) {
+      print(" number one id biggest than another");
+    } else {
+      print("number three is biggest than other");
+    }
+  } else if (num2 > num3) {
+    print("number two is biggest than other");
+  }
 }
